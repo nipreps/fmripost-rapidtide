691cf39cef2380b9d9f4c0cadf8ea543ffe52694
Taylor Salo <tsalo006@fiu.edu>
2024-09-04 09:12:32 -0400

    Update test_base.py

--- src/fmripost_rapidtide/tests/test_base.py
+++ src/fmripost_rapidtide/tests/test_base.py
@@ -20,18 +20,9 @@ def test_init_rapidtide_wf(tmp_path_factory):
         wf = init_rapidtide_wf(
             bold_file='sub-01_task-rest_bold.nii.gz',
             metadata={'RepetitionTime': 2.0},
+            mem_gb={
+                'resampled': 1,
+                'largemem': 2,
+            },
         )
         assert wf.name == 'rapidtide_task_rest_wf'
-
-
-def test_init_denoise_wf(tmp_path_factory):
-    from fmripost_rapidtide.workflows.rapidtide import init_denoise_wf
-
-    tempdir = tmp_path_factory.mktemp('test_init_denoise_wf')
-
-    with mock_config():
-        config.execution.output_dir = tempdir / 'out'
-        config.execution.work_dir = tempdir / 'work'
-
-        wf = init_denoise_wf(bold_file='sub-01_task-rest_bold.nii.gz')
-        assert wf.name == 'denoise_task_rest_wf'
